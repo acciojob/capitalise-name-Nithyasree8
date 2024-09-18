c70a4913fc711b6d943af6184121e1bb3d554f75
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-09-18 18:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required.
-const inputFiled=document.getElementbyId("fname");
-inputField.addEventlistener('blur',()=> {
-	inputField.value =inputField.value.toUppercase();
+document.addEventListener('DOMContentLoaded', function() {
+    const inputField = document.getElementById('fname');
+    inputField.addEventListener('blur', function() {
+        inputField.value = inputField.value.toUpperCase();
+    });
 });
